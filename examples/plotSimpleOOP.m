83a26ca035ec1ac0066e7e86e8bbbf95bf1d8539
K M Masum Habib <masum.habib@gmail.com>
2014-09-23 17:24:37 -0400

    Plot class implemented. No marker support yet. Added one example.

---
+++ examples/plotSimpleOOP.m
@@ -0,0 +1,37 @@
+% Load, modify and export a fig file as an eps file.
+
+clear all;
+addpath('../lib');
+
+%% lets plot 3 cycles of 50Hz AC voltage
+f = 50;  % frequency
+Vm = 10; % peak
+phi = 0; % phase
+
+% generate the signal
+t = [0:0.0001:3/f];
+th = 2*pi*f*t;
+v = Vm*sin(th+phi);
+
+% plot it
+figure;
+plot(t*1E3, v);
+
+p = Plot();
+
+% p.Title = 'Voltage as a function of time'; % plot title
+p.XLabel = 'Time, t (ms)'; % xlabel
+p.YLabel = 'Voltage, V (V)'; %ylabel
+
+
+% % change settings
+% opt = [];
+% opt.XLabel = 'Time, t (ms)'; % xlabel
+% opt.YLabel = 'Voltage, V (V)'; %ylabel
+% 
+% % Save? comment the following line if you do not want to save
+% opt.FileName = 'plotSimple1.png'; 
+% 
+% % apply the settings
+% setPlotProp(opt);
+    
